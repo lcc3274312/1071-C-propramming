82e70a02f6f160b454964f19687d1612c4d546f9
lcc3274312 <43335022+lcc3274312@users.noreply.github.com>
2018-10-02 16:22:39 +0800

    Create sum fun.cpp

---
+++ wo4/sum fun.cpp
@@ -0,0 +1,46 @@
+#include<stdio.h>
+int sum(int n1,int n2)  //function prototype
+{
+    int total =0;
+    for(int i=n1 ; i<=n2 ; i++)
+    {
+        total +=i;
+    }
+    return total;
+}
+
+int main()
+{
+    int n1,n2;
+    int total=0;
+    printf("Enter n1 n2 : ");
+    scanf("%d %d",&n1,&n2);
+    while(n1>=0 && n2>=0)
+    {
+        total = sum(n1,n2);
+        printf("sum(%d,%d) = %d\n",n1,n2,total);
+        printf("Enter n1 n2 ");
+        scanf("%d %d",&n1,&n2);
+    }
+    return 0;
+}
+
+
+/*
+int main(){
+    int n1,n2;
+    int sum=0;
+    printf("Enter n1 n2 :");
+    scanf("%d %d",&n1,&n2);
+    while(n1>=0 || n1>=0){
+		int sum=0;
+		for(int i=n1;i<=n2;i++){
+		    sum += i;
+		    }
+	    printf("sum(%d,%d)=%d\n",n1,n2,sum);
+	    printf("Enter n1 n2 :");
+	    scanf("%d %d",&n1,&n2);
+
+    }
+}
+*/
